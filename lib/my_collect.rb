81e18e2934d9992ee04a5a63c6a2eff7a928ca4e
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-11 11:59:12 -0700

    commit tree fbbc747ad8b2304b12feaceab924328dcc39304e

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(array)
+collection = []
+x = 0 
+if block_given? 
+    while x < array.length  
+        collection << yield(array[x])
+        x+=1
+    end 
+else 
+    "None"
+end 
+ collection 
+end  
+
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) do |languages|
+    languages.upcase
+end 
+
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |names|
+    names.split(" ").first
+end
 
 
